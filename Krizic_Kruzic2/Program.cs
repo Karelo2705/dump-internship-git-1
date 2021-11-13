5d8b7d07cf70f1f1ddf2c8a11b743fdf4e8a8bcb
Karelo2705 <karlo.blaskovic@gmail.com>
2021-11-13 23:26:44 +0100

    mislin da je to to

--- Krizic_Kruzic2/Program.cs
+++ Krizic_Kruzic2/Program.cs
@@ -6,8 +6,8 @@ namespace Krizic_Kruzic2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mirna ima x pobjeda i y poraza!");
-            Console.WriteLine("Karlo ima z pobjeda i n poraza!");
+            Console.WriteLine("Mirna ima 3 pobjeda i 0 poraza!");
+            Console.WriteLine("Karlo ima 0 pobjeda i 3 poraza!");
 
             Console.WriteLine(" X |   |  O");
             Console.WriteLine(" X | O |   ");
