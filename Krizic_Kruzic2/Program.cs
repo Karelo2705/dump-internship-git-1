b037e19bfc47d52b625aafa6d72d49546031caeb
Karelo2705 <karlo.blaskovic@gmail.com>
2021-11-13 22:08:49 +0100

    Resetirano

--- Krizic_Kruzic2/Program.cs
+++ Krizic_Kruzic2/Program.cs
@@ -11,7 +11,7 @@ namespace Krizic_Kruzic2
 
             Console.WriteLine("   |   |   ");
             Console.WriteLine("   |   |   ");
-            Console.WriteLine(" X |   |   ");
+            Console.WriteLine("   |   |   ");
         }
     }
 }
